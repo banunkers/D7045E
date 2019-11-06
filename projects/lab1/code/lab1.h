70a3fdb6177db5a3fd9451c17dc9888cc72a5271
banunkers <hugwan-6@student.ltu.se>
2019-11-06 20:18:45 +0100

    Fixed build task

--- projects/lab1/code/lab1.h
+++ projects/lab1/code/lab1.h
@@ -3,26 +3,22 @@
 #include "core/app.h"
 #include "render/window.h"
 
-namespace Lab1
-{
-class Lab1App : public Core::App
-{
-public:
-	/// constructor
-	Lab1App();
-	/// destructor
-	Lab1App();
-
-	/// open app
-	bool Open();
-	/// run app
-	void Run();
-private:
-
-	GLuint program;
-	GLuint vertexShader;
-	GLuint pixelShader;
-	GLuint triangle;
-	Display::Window* window;
-};
+namespace Lab1 {
+	class Lab1App: public Core::App {
+		public:
+			/// constructor
+			Lab1App();
+			/// destructor
+			~Lab1App();
+			/// open app
+			bool Open();
+			/// run app
+			void Run();
+		private:
+			GLuint program;
+			GLuint vertexShader;
+			GLuint pixelShader;
+			GLuint triangle;
+			Display::Window* window;
+	};
 }
