6b9941800fc88db8894298ebadb8d286787b159a
banunkers <hugwan-6@student.ltu.se>
2019-11-07 11:39:12 +0100

    Reworked example project to now handle vertices and indices

--- projects/lab1/code/lab1.cc
+++ projects/lab1/code/lab1.cc
@@ -1,5 +1,6 @@
 #include "config.h"
 #include "lab1.h"
+#include <vector>
 #include <cstring>
 #include <glm/glm.hpp>
 #include <glm/vec3.hpp> // glm::vec3
@@ -29,6 +30,12 @@ const GLchar* ps =
 "	Color = color;\n"
 "}\n";
 
+const GLuint vertex_attrib_index = 0;
+const GLuint color_attrib_index = 1;
+const GLuint vertex_record = 7 * sizeof(float32);
+const GLuint vertex_offset = 0 * sizeof(float32);
+const GLuint color_offset = 4 * sizeof(float32);
+
 using namespace Display;
 namespace Lab1 {
 	Lab1App::Lab1App() {
@@ -49,14 +56,21 @@ namespace Lab1 {
 		window->SetTitle(std::string("Lab1: 2D Koch Snowflake"));
 		
 		
-		GLfloat buf[] = {
+		this->vertices = {
 			-0.5f,	-0.5f,	-1,			// pos 0
-			1,		0,		0,		1,	// color 0
+			0,		0,		1,		1,	// color 0
 			0,		0.5f,	-1,			// pos 1
-			0,		1,		0,		1,	// color 0
+			0,		0,		1,		1,	// color 1
 			0.5f,	-0.5f,	-1,			// pos 2
-			0,		0,		1,		1	// color 0
+			0,		0,		1,		1,	// color 2
+			// 0.75f,	0.75f,	-1,			// pos 3
+			// 0,		0,		1,		1,	// color 3
 		};
+		this->indices = {
+			0, 1, 2,
+			// 1, 3, 2
+		};
+	
 
 		if (this->window->Open()) {
 			// set clear color to pale yellow
@@ -106,12 +120,6 @@ namespace Lab1 {
 				printf("[PROGRAM LINK ERROR]: %s", buf);
 				delete[] buf;
 			}
-
-			// setup vbo
-			glGenBuffers(1, &this->triangle);
-			glBindBuffer(GL_ARRAY_BUFFER, this->triangle);
-			glBufferData(GL_ARRAY_BUFFER, sizeof(buf), buf, GL_STATIC_DRAW);
-			glBindBuffer(GL_ARRAY_BUFFER, 0);
 			return true;
 		}
 		return false;
@@ -123,22 +131,54 @@ namespace Lab1 {
 			glClear(GL_COLOR_BUFFER_BIT);
 			this->window->Update();
 
-			// do stuff
-			glBindBuffer(GL_ARRAY_BUFFER, this->triangle);
-			glUseProgram(this->program);
-			glEnableVertexAttribArray(0);
-			glEnableVertexAttribArray(1);
-			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float32) * 7, NULL);
-			glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float32) * 7, (GLvoid*)(sizeof(float32) * 3));
-			glDrawArrays(GL_TRIANGLES, 0, 3);
+			// Koch config
+			const int depth = 1;
+			const int num_edges = 3*(pow(4, depth));
+			// The number of vertex in Koch Snowflake is the number of edges * 2
+			const int num_vertex = 2*num_edges;
+						
+			// Get the vertices and indices of the koch snowflake
+			// std::pair<std::vector<GLfloat>, std::vector<GLuint>> res = koch_snowflake(&this->vertices, &this->indices, 0);
+			// this->vertices = res.first;
+			// this->indices = res.second;
+			koch_snowflake(this->vertices, this->indices, 0);
+
+			// Bind the new data to the buffer objects
+			// setup vbo (vertex buffer object)
+			glGenBuffers(1, &this->vbo);
+			glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
+			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), vertices.data(), GL_STATIC_DRAW);
 			glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+			// setup ibo (index buffer object)
+			glGenBuffers(1, &this->ibo);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->ibo);
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(GLuint), indices.data(), GL_STATIC_DRAW);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+			// Draw the snowflake at specified depth			
+			glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
+			glUseProgram(this->program);
+			glEnableVertexAttribArray(vertex_attrib_index);
+			glEnableVertexAttribArray(color_attrib_index);
+			glVertexAttribPointer(vertex_attrib_index, 3, GL_FLOAT, GL_FALSE, vertex_record, (GLvoid*)(vertex_offset));
+			glVertexAttribPointer(color_attrib_index, 4, GL_FLOAT, GL_FALSE, vertex_record, (GLvoid*)(color_offset));
+			
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->ibo);
+			glDrawElements(GL_TRIANGLES, (GLsizei)indices.size(), GL_UNSIGNED_INT, nullptr);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 			this->window->SwapBuffers();
 		}
 	}
 
-	void koch_snowflake(int depth) {
-		
+	// Calculates and returns the vertexes and indices given a recursion depth and starting vectors
+	// std::pair<std::vector<GLfloat>, std::vector<GLuint>>
+	void koch_snowflake(vector<GLfloat> vertices, vector<GLuint> indices, int depth) {
+			if (depth == 0) {
+				return;
+			} else {
+				
+			}
 	}
-
 }
--- projects/lab1/code/lab1.h
+++ projects/lab1/code/lab1.h
@@ -2,6 +2,9 @@
 
 #include "core/app.h"
 #include "render/window.h"
+#include <vector>
+
+using namespace std;
 
 namespace Lab1 {
 	class Lab1App: public Core::App {
@@ -18,7 +21,13 @@ namespace Lab1 {
 			GLuint program;
 			GLuint vertexShader;
 			GLuint pixelShader;
-			GLuint triangle;
+			GLuint vbo;
+			GLuint ibo;
+			vector<GLfloat> vertices;
+			vector<GLuint> indices;
 			Display::Window* window;
 	};
+
+	// std::pair<std::vector<GLfloat>, std::vector<GLuint>> koch_snowflake(vector<GLfloat> *vertices, vector<GLuint> *indices, int depth);
+	void koch_snowflake(vector<GLfloat> vertices, vector<GLuint> indices, int depth);
 }
