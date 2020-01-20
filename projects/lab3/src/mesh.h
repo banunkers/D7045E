68ace306be04dfee65a16bbc9db44ab6739c096d
banunkers <hugwan-6@student.ltu.se>
2020-01-20 11:05:27 +0100

    implemented mesh class

---
+++ projects/lab3/src/mesh.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <glm.hpp>
+#include <vector>
+
+#include "glew.h"
+#include "vertex.h"
+
+class Mesh {
+    private:
+        GLuint vertexArrayObject;
+        GLuint vertexBufferObject;
+        GLuint elementBufferObject;
+        std::vector<Vertex>* vertices;
+        std::vector<GLuint>* indices;
+
+        void initMesh() {
+            // setup VAO
+            glGenVertexArrays(1, &this->vertexArrayObject);
+            glBindVertexArray(this->vertexArrayObject);
+
+            // setup VBO
+            glGenBuffers(1, &this->vertexBufferObject);
+            glBindBuffer(GL_ARRAY_BUFFER, this->vertexBufferObject);
+            glBufferData(GL_ARRAY_BUFFER, this->vertices->size() * sizeof(Vertex), this->vertices->data(), GL_STATIC_DRAW);
+
+            // setup EBO
+            glGenBuffers(1, &this->elementBufferObject);
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->elementBufferObject);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, this->indices->size() * sizeof(GLuint), this->indices->data(), GL_STATIC_DRAW);
+
+            // setup vertex attrib pointers
+            // position
+            glEnableVertexAttribArray(0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+        }
+
+    public:
+        Mesh(std::vector<Vertex>* vertices, std::vector<GLuint>* indices) {
+            this->vertices = vertices;
+            this->indices = indices;
+            initMesh();
+        }
+
+        ~Mesh() {
+            glDeleteVertexArrays(1, &this->vertexArrayObject);
+            glDeleteBuffers(1, &this->vertexBufferObject);
+            glDeleteBuffers(1, &this->elementBufferObject);
+        }
+};
---
+++ projects/lab3/src/vertex.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <glm.hpp>
+
+struct Vertex {
+    glm::vec3 position;
+};
